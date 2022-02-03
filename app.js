06764a9ad8f3092a82c3789efb31f1ebf338b5cd
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-02 18:17:56 -0800

    set event listener for adding new enemy logged for confirmation of working state

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions and grab DOM elements
+const form = document.getElementById('enemy-name-form');
 const countEl = document.getElementById('dead-enemy-count');
 const spawnEl = document.getElementById('spawner');
 const playerHpEl = document.getElementById('player-hp');
@@ -11,8 +12,27 @@ let enemies = [
     { id: 1, name: 'Sephiroth', hp: 10000 },
     { id: 2, name: 'Cactaur', hp: 1 },
 ];
+let enemyId = 3;
 
 // set event listeners
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //grab the form
+    const data = new FormData(form);
+    //use the form variable to get the user input
+    const enemyName = data.get('enemy-name');
+    //make enemy object with form data
+    //increment id
+    const newEnemy = {
+        id: enemyId++,
+        name: enemyName,
+        hp: Math.ceil(Math.random() * 7),
+    };
+    //add to enemy array
+    enemies.push(newEnemy);
+    console.log(newEnemy, enemies, 'check');
+    //display--cant do till function is written
+});
 // get user input
 // use user input to update state
 // update DOM to reflect the new state
